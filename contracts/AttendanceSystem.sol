718895b8698443dc8e946096e7f54d8368864938
warrencxw <69459230+warrencxw@users.noreply.github.com>
2023-10-30 10:56:00 +0800

    Update AttendanceSystem.sol to let it function correctly
    Made following changes
    1. Removed few functions
    2. Simplified and corrected code for markMyAttendance() and startNewAttendance()
    3. Add setter function for attendanceAmount
    4. Add getter functions

--- contracts/AttendanceSystem.sol
+++ contracts/AttendanceSystem.sol
@@ -6,60 +6,57 @@ contract AttendanceSystem {
     StreamerChannel streamerChannelContract;
     uint256 attendanceAmount;
     uint256 timeLimit;
-    bool allowAttending;
     mapping (address => uint256) viewerAttendance;
     address [] viewers;
 
     address owner;
     constructor(address streamerContractAddress) public {
         streamerChannelContract = StreamerChannel(streamerContractAddress);
+        attendanceAmount = 30; // Default value, call setAttendanceAmount() to change
     }
 
-    modifier onlyStreamer(address callAddr) {
-        require(streamerChannelContract.getStreamer() == callAddr, "Only Streamer are allowed to startNewAttendance");
+    modifier onlyStreamer() {
+        require(streamerChannelContract.getStreamer() == msg.sender, "Only Streamer are allowed to startNewAttendance");
         _;
     }
-    modifier  allowsAttending(uint256 callTime) {
+    modifier allowsAttending(uint256 callTime) {
         require(callTime < timeLimit, "Streamer has stopped streaming or the time slot has ran out");
+        require(viewerAttendance[msg.sender] < timeLimit, "Attendance has already been marked");
         _;
     }
 
-    function exists(address addr) private view returns (bool) {
-        if (viewers.length > 0) {
-            for (uint i = 0; i < viewers.length; i++) {
-                if (viewers[i] == addr) {
-                    return true;
-                }
-            }
-        }
-        
-        return false;
+    modifier validAmount(uint256 amount) {
+        require(amount >= 0, "The number of tokens awarded for attendance cannot be less than 0");
+        _;
     }
 
+    function setAttendanceAmount(uint256 newAmount) public onlyStreamer validAmount(newAmount) {
+        attendanceAmount = newAmount;
+    }
+
+    function getAttendanceAmount() public view returns(uint256) {
+        return attendanceAmount;
+    }
 
     function markMyAttendance() public allowsAttending(block.timestamp) {
         // If the user has clicked this button within the time range then set their amount
         // Also needs to make sure the user hasn't already marked Their Attendance
-        if (exists(msg.sender)) {
-            require(viewerAttendance[msg.sender] == 0, "Attendance already marked");
-        }
-        attendanceAmount =  timeLimit - block.timestamp;
         streamerChannelContract.issueTokens(msg.sender, attendanceAmount);
-        viewerAttendance[msg.sender] = block.timestamp;
-        viewers.push(msg.sender);
+        viewerAttendance[msg.sender] = timeLimit;
     }
 
-    function resetBalance() private  {
-        for (uint i=0; i < viewers.length ; i++){
-            viewerAttendance[viewers[i]] = 0;
-        }
-    }
-
-    function startNewAttendance(uint attendancePeriod) public onlyStreamer(msg.sender) {
+    function startNewAttendance(uint attendancePeriod) public onlyStreamer {
         // Attendance period is taken in minutes, so if value is 30, it means 30 minutes
         // This function allows streamers to set the timeperiod in which people can come in and mark their attendance
         // Between that time period anyone will be able to call markMyAttendance
-        resetBalance();
         timeLimit = block.timestamp + (attendancePeriod*60);
+    }
+
+    function getTimeLimit() public view returns(uint256) {
+        return timeLimit;
+    }
+
+    function getCurrentTime() public view returns(uint256) {
+        return block.timestamp;
     }
 }
