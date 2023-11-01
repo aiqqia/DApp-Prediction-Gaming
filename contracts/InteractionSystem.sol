ffc2b6ed3045e015ee816a0d6d570e9497afce83
warrencxw <69459230+warrencxw@users.noreply.github.com>
2023-11-01 19:07:22 +0800

    Make minor edits to InteractionSystem.sol
    1. Removed the streamer check for deploying contract
    2. Changed subscription to mapping(address=>uint256), mapping address to duration of subscription
    3. Removed subscriberNotExist modifier and checks, subscription are extended on resubscription
    4. Shifted subscriptionFee to be contract wide variable
    5. Added subscriptionDuration contract wide variable
    6. Added code to pay back the user if they paid in excess of the subscriptionFee
    7. Changed donors from address[] to mapping(address=>uint256), map address to amount of $ donated
    8. Removed printAllDonors function
    9. Removed InteractionPeek event to make peekNextInteraction a view function
    10. Made popNextInteraction return exactly the same data as peekNextInteraction
    11. Reordered the code to be Definitions -> Viewer-use Functions -> Streamer-use Functions -> Utility functions that might be called by other systems for readability sake
    12. Added getSubscriptionFee, getSubscriptionDuration, setSubscriptionFee, setSubscriptionDuration, isSubscribed and isDonor functions

--- contracts/InteractionSystem.sol
+++ contracts/InteractionSystem.sol
@@ -5,21 +5,23 @@ import "./StreamerChannel.sol"; // Import the StreamChannel interface
 
 contract InteractionSystem{
     StreamerChannel streamerChannelContract;
-    address[] donors;
+    uint256 subscriptionFee = 1 ether; // Subscription fee default at 1 ether
+    uint256 subscriptionDuration = 30 * 24 * 60 * 60; // Subscription duration default at 30 days
      
-    mapping(address => bool) public subscribers;
+    mapping(address => uint256) public subscribers; // Maps address to date
+    mapping(address => uint256) public donors; // Maps address to amount donated
 
     struct InteractionType {
         uint256 cost;
         string description;
     }
 
-    InteractionType[] public interactions;
-
     struct ViewerInteractionRequest {
         uint256 interactionTypeIndex;
         address requester;
     }
+
+    InteractionType[] public interactions;
     ViewerInteractionRequest[] public viewerInteractionRequestQueue;    
 
     uint256 currentViewerQueueIndex = 0;
@@ -27,7 +29,6 @@ contract InteractionSystem{
 
     event InteractionRequested(address requester, uint256 interactionCost, string description);
     event InteractionAdded(uint256 interactionCost, string interactionDescription);
-    event InteractionPeek(uint256 streamerCompletedQueueIndex, uint256 InteractionType, address requester);
     event InteractionPop(uint256 InteractionType, address requester);
     event DonorAdded(address donor);
     event SubscriberAdded(address subscriber);
@@ -35,11 +36,6 @@ contract InteractionSystem{
 
     constructor(address streamChannelAddress) public {
         streamerChannelContract = StreamerChannel(streamChannelAddress);
-        address streamer = streamerChannelContract.getStreamer();
-        require(streamer == msg.sender, "Only Streamer can deploy this contract!");
-        
-        streamerChannelContract.setInteractionSystemContract(address(this));
-
     }
 
     modifier onlyStreamer() {
@@ -52,34 +48,40 @@ contract InteractionSystem{
         _;
     }
 
-    modifier subscriberNotExists(address viewer) {
-        require(!subscribers[viewer], "Subscriber already exists");
+    modifier isEnoughSubscriptionFeeProvided(uint256 value) {
+        require(msg.value >= subscriptionFee, "The amount provided is not enough for the subscription!");
         _;
     }
 
-    modifier isEnoughSubscriptionFeeProvided(uint256 value){
-        //subscription fee 1 eth
-        uint256 subscriptionFee = 1 ether;
-
-        //require msg.value >= 1 eth
-        require(msg.value >= subscriptionFee, "Subscription requires at least 1 ether!");
-        _;
-    }
-
-    modifier isEnoughTokenBalance(address viewer, uint256 interactionTypeIndex){
+    modifier isEnoughTokenBalance(address viewer, uint256 interactionTypeIndex) {
         //require viewer token balance >= interactionCost
-        require(streamerChannelContract.getViewerTokens(viewer) >= interactions[interactionTypeIndex].cost, "Viewer should have enough token balance!");
+        require(streamerChannelContract.getViewerTokens(viewer) >= interactions[interactionTypeIndex].cost, "Viewer does not have enough token balance for requested interaction!");
         _;
     }
 
-    modifier isInteractionQueueEmpty(){
-        require(currentViewerQueueIndex != streamerCompletedQueueIndex, "Interaction queue is empty currently...");
+    modifier isInteractionQueueEmpty() {
+        require(currentViewerQueueIndex != streamerCompletedQueueIndex, "Interaction queue is currently empty...");
         _;
     }
 
     modifier isValidInteractionType(uint256 interactionTypeIndex) {
         require(interactionTypeIndex >= 0 && interactionTypeIndex < interactions.length, "Invalid interaction type");
         _;
+    }
+
+    // -- FUNCTIONS FOR VIEWERS --
+    // print all interaction types available for viewers
+    function getAllInteractionTypes() public view returns (uint256[] memory costs, string[] memory descriptions) {
+        uint256 length = interactions.length;
+        costs = new uint256[](length);
+        descriptions = new string[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            costs[i] = interactions[i].cost;
+            descriptions[i] = interactions[i].description;
+        }
+
+        return (costs, descriptions);
     }
 
     function requestInteraction(uint256 interactionTypeIndex) public 
@@ -101,89 +103,88 @@ contract InteractionSystem{
         emit InteractionRequested(msg.sender, interactionCost, interactionDescription);
     }
 
-    function makeDonation() public payable donationGreaterThanZero(msg.value){
-
+    function makeDonation() public payable donationGreaterThanZero(msg.value) {
         address streamer = streamerChannelContract.getStreamer();
         address payable payableStreamer = address(uint160(streamer));
-
-        //check if alrdy in donors
-        for(uint256 i=0; i < donors.length; i++){
-            address donor = donors[i];
-            if(donor == msg.sender){
-                //transfer ether to the streamer
-                payableStreamer.transfer(msg.value);
-                return;
-            }
-        }
-        //add to donors
-        donors.push(msg.sender);
-
-        //transfer ether to the streamer
         payableStreamer.transfer(msg.value);
-
+        
+        //add to donors
+        donors[msg.sender] += msg.value;
         emit DonorAdded(msg.sender);
     }
 
-    function subscribe() public payable subscriberNotExists(msg.sender) isEnoughSubscriptionFeeProvided(msg.value) {
+    function getSubscriptionFee() public view returns(uint256) {
+        return subscriptionFee;
+    }
 
-        // add to subscribers
-        subscribers[msg.sender] = true;
+    function getSubscriptionDuration() public view returns(uint256) {
+        return subscriptionDuration;
+    }
 
+    function subscribe() public payable isEnoughSubscriptionFeeProvided(msg.value) {
         //transfer ether to the streamer
         address streamer = streamerChannelContract.getStreamer();
         address payable payableStreamer = address(uint160(streamer));
-        payableStreamer.transfer(msg.value);
+        payableStreamer.transfer(subscriptionFee);
+        address(uint160(msg.sender)).transfer(msg.value - subscriptionFee); // Return remaining after charging subscription fee
+
+        // add to subscribers
+        if (subscribers[msg.sender] < block.timestamp) {
+            subscribers[msg.sender] = block.timestamp + subscriptionDuration;
+        } else {
+            subscribers[msg.sender] += subscriptionDuration;
+        }
 
         emit SubscriberAdded(msg.sender);
-
     }
 
+    // -- FUNCTIONS FOR STREAMERS --
     // function to allow only streamers to add types of interaction available
-    function addInteraction(uint256 cost, string memory description) public onlyStreamer{
+    function addNewInteraction(uint256 cost, string memory description) public onlyStreamer {
         interactions.push(InteractionType(cost, description));
         emit InteractionAdded(cost, description);
     }
 
     // for streamer to view the next viewer's request in interaction queue
-    function peekNextInteraction() public onlyStreamer isInteractionQueueEmpty 
-        returns(uint256, string memory, address)    
-    {
+    function peekNextInteraction() public view onlyStreamer isInteractionQueueEmpty 
+        returns(uint256, string memory, address) {
         uint256 interactionTypeIndex = viewerInteractionRequestQueue[streamerCompletedQueueIndex].interactionTypeIndex;
         address requester = viewerInteractionRequestQueue[streamerCompletedQueueIndex].requester;
-        emit InteractionPeek(streamerCompletedQueueIndex, interactionTypeIndex, requester);
+
         return (interactionTypeIndex, interactions[interactionTypeIndex].description, requester);
     }
 
     // for streamer to proceed to the next request in the queue
-    function popNextInteraction() public onlyStreamer isInteractionQueueEmpty{
+    function popNextInteraction() public onlyStreamer isInteractionQueueEmpty 
+        returns(uint256, string memory, address) {
         uint256 interactionTypeIndex = viewerInteractionRequestQueue[streamerCompletedQueueIndex].interactionTypeIndex;
         address requester = viewerInteractionRequestQueue[streamerCompletedQueueIndex].requester;
         streamerCompletedQueueIndex += 1;
 
         emit InteractionPop(interactionTypeIndex, requester);
+        return (interactionTypeIndex, interactions[interactionTypeIndex].description, requester);
     }
 
     function getStreamerCompletedQueueIndex() public view returns (uint256) {
         return streamerCompletedQueueIndex;
     }
 
-    // print all donors
-    function getAllDonors() public {
-        emit AllDonors(donors);
+    function setSubscriptionFee(uint256 newSubscriptionFee) public onlyStreamer {
+        require(newSubscriptionFee > 0, "Subscription fee should be a value > 0 in terms of wei.");
+        subscriptionFee = newSubscriptionFee;
     }
 
-    // print all interaction types available for viewers
-    function getAllInteractionTypes() public view returns (uint256[] memory costs, string[] memory descriptions) {
-        uint256 length = interactions.length;
-        costs = new uint256[](length);
-        descriptions = new string[](length);
-
-        for (uint256 i = 0; i < length; i++) {
-            costs[i] = interactions[i].cost;
-            descriptions[i] = interactions[i].description;
-        }
-
-        return (costs, descriptions);
+    function setSubscriptionDuration(uint256 newSubscriptionDuration) public onlyStreamer {
+        require(newSubscriptionDuration > 0, "Subscription duration should be a value > 0 in terms of seconds.");
+        subscriptionDuration = newSubscriptionDuration; // Value in seconds
     }
 
+    // -- UTILITY FUNCTIONS --
+    function isSubscribed(address viewer) public view returns (bool) {
+        return subscribers[viewer] > block.timestamp;
+    }
+
+    function isDonor(address viewer) public view returns (bool) {
+        return donors[viewer] > 0;
+    }
 }
